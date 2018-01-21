da9098af8d7bcedb1cdff43f5208cec01b9035f9
Ethan Kurtz <ethan@hungryraccoon.com>
2018-01-21 07:38:46 -0800

    Adding existing progress to coding test
    Please see readme file for updates.

--- src/main/java/com/assignment/microservice/MicroserviceApplication.java
+++ src/main/java/com/assignment/microservice/MicroserviceApplication.java
@@ -1,12 +1,57 @@
 package com.assignment.microservice;
 
+import java.io.File;
+import java.io.IOException;
+
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringBootConfiguration;
 
-@SpringBootApplication
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+
+
+@SpringBootConfiguration
 public class MicroserviceApplication {
+	private static String bucketName     = "niketest-ek";
+	private static String keyName        = "AKIAIJOTP2MZYM5JWXJQ";
+	private static String secretKey      = "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";
+	private static String uploadFileName = "C:\\Users\\LarBoy\\Documents\\niketest.txt";
+	static BasicAWSCredentials credentials = new BasicAWSCredentials(keyName, secretKey);
+	static AmazonS3 s3Client = AmazonS3ClientBuilder.standard().withCredentials(new AWSStaticCredentialsProvider(credentials)).build();
+	
+	public static void main(String[] args) throws IOException {
+		 
+       try {
+            System.out.println("Uploading a new object to S3 from a file\n");
+            File file = new File(uploadFileName);
+            s3Client.putObject(new PutObjectRequest(
+            		                 bucketName, keyName, file));
 
-	public static void main(String[] args) {
-		SpringApplication.run(MicroserviceApplication.class, args);
-	}
+         } catch (AmazonServiceException ase) {
+            System.out.println("Caught an AmazonServiceException, which " +
+            		"means your request made it " +
+                    "to Amazon S3, but was rejected with an error response" +
+                    " for some reason.");
+            System.out.println("Error Message:    " + ase.getMessage());
+            System.out.println("HTTP Status Code: " + ase.getStatusCode());
+            System.out.println("AWS Error Code:   " + ase.getErrorCode());
+            System.out.println("Error Type:       " + ase.getErrorType());
+            System.out.println("Request ID:       " + ase.getRequestId());
+        } catch (AmazonClientException ace) {
+            System.out.println("Caught an AmazonClientException, which " +
+            		"means the client encountered " +
+                    "an internal error while trying to " +
+                    "communicate with S3, " +
+                    "such as not being able to access the network.");
+            System.out.println("Error Message: " + ace.getMessage());
+        }
+    }
 }
---
+++ src/main/java/com/assignment/microservice/readme.md
@@ -0,0 +1,7 @@
+I believe all of the needed spring and aws classes are connected now. The issue I'm having is that the running of the application keeps hanging up on needed a variety of additional libraries (most specifically from Jackson).  Is this the reference you made to the spring annotations library? In any event, it feels like I could be chasing libraries all day to have them all included and I feel like there is a better way.
+
+I think the code to upload the file will work, once I get the issue with the libraries resolved.
+
+I will send an email with the jar files attached that I have had to add to the class path so far.
+
+Thanks again - I'm learning a lot about the application framework through this project.
